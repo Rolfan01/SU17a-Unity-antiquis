79fc32ff00a871d01b792b1a1100e3af9544671b
Lemongrabz <35961561+Lemongrabz@users.noreply.github.com>
2018-01-30 15:27:06 +0100

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+
+    public Transform target;
+    public float speed = 3f;
+    public float attack1Range = 1f;
+    public int attack1Damage = 1;
+    public float timeBetweenAttacks;
+
+
+
+	// Use this for initialization
+	void Start () {
+        Rest();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void MoveToPlayer()
+    {
+        transform.LookAt(target.position);
+        transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+        if (Vector3.Distance (transform.position, target.position) > attack1Range)
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+    public void Rest ()
+    {
+
+    }
+}
+    
---
+++ EnemyInTerritory.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInTerritory : MonoBehaviour
+{
+
+
+
+    public BoxCollider2D territory;
+    GameObject player;
+    bool playerInTerritory;
+
+    public GameObject enemy;
+    Enemy basicEnemy;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        basicEnemy = enemy.GetComponent<Enemy>();
+        playerInTerritory = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInTerritory = true)
+        {
+            basicEnemy.MoveToPlayer();
+
+        }
+        if (playerInTerritory = false)
+        {
+            basicEnemy.Rest();
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+            if (other.gameObject == player)
+        {
+            playerInTerritory = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInTerritory = false;
+        }
+    }
+
+}
