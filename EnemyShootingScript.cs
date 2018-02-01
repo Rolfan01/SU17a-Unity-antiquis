701ba04289152a9577892de259fd505fbf79878e
Edgar Wretman <edewretman@hotmail.com>
2018-02-01 09:28:40 +0100

    Add files via upload

---
+++ EnemyShootingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootingScript : MonoBehaviour
+{
+    public float speed;
+    Transform player;
+
+     
+    void Start()
+    {
+        // Add + 1 to player's last known position so bullet appears to float above ground.
+        Vector3 playerPos = new Vector3(player.position.x, player.position.y + 1, player.position.z);
+
+        // Aim bullet in player's direction.
+        transform.rotation = Quaternion.LookRotation(playerPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the projectile forward towards the player's last known direction;
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+}
