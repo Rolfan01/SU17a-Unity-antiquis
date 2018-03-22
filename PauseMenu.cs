1983ed7d08399c647567ef9fc2c6fe65203ad921
Gelatinousso <35961089+Gelatinousso@users.noreply.github.com>
2018-03-22 11:24:50 +0100

    PauseMenu

---
+++ PauseMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() // Use on ResumeButton with On Click
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void LoadMenu() // Use on MenuButton with On Click
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Loading menu...");
+
+    }
+    public void QuitGame() // Use on QuitButton with On Click
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    void Pause() 
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
